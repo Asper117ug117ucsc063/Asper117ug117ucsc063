db5cebfe22d7bf026d74ed5f0cf0a17de3891ee1
Asper117ug117ucsc063 <144677539+Asper117ug117ucsc063@users.noreply.github.com>
2023-09-24 19:32:47 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,29 @@
-class bankaccount:
+class Student:
 
-  def __init__(self,account_number,account_holder_name,initial_balance=0.0):
-    self.__account_number = account_number
-    self.__account_holder_name = account_holder_name
-    self.__account_balance = initial_balance
-
-  def deposit(self , amount):
-    if amount > 0:
-      self.__account_balance += amount
-      print("Deposited ₹{}. New balance: ₹{}".format(amount,self.__account_balance))
-
-    else:
-      print("Invalid deposit amount. Please deposit a positive amount.")
-
-  def withdraw(self, amount):
-    if amount > 0 and amount <= self.__account_balance:
-      self.__account_balance -= amount
-      print("Withdraw ₹{}. New balance: ₹{}".format(amount,self.__account_balance))
-
-    else:
-      print("Invalid withdrawl amount or insufficient balance.")
-
-  def display_balance(self):
-    print("Account balance for {} (Account #{}): ₹{}".format(
-       self.__account_holder_name,self.__account_number,self.__account_balance))
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
 
 
-# Create an instance of the Bankaccount class
-account = bankaccount(account_number="123456789",account_holder_name="Vishwa",initial_balance=5000.0)
+def sort_students(student_list):
+  # Sort the list of students in descending order of CGPA
+  sorted_students = sorted(student_list,key=lambda student: student.cgpa,reverse=True)
+  return sorted_students
 
-# Test deposit and withdrawal functionality
-account.display_balance()
-account.deposit(500.0)
-account.withdraw(200.0)
-account.display_balance()
+
+# Example usage:
+students = [
+  Student("Hari", "A123", 7.8),
+  Student("Srikanth", "A124", 8.9),
+  Student("Saumya", "A125", 9.1),
+  Student("Mahidhar", "A126", 9.9),
+]
+
+sorted_students = sort_students(students)
+
+# Prime the sorted list of students
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA {}".format(student.name,
+                                                    student.roll_number,
+                                                    student.cgpa) )
